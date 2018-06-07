ede510644e416a6c7cc1603fb896c6c68bac5305
VBurt <ChargerIIC@yahoo.com>
2018-06-07 10:06:52 -0700

    Additional work on Product
    Additional work to match the data model

--- src/model/product.model.ts
+++ src/model/product.model.ts
@@ -1,15 +1,15 @@
 import { Version } from "./version.model";
 
 export class Product{
+    public Id: number; //arbitary id for sort order
+
     public Title : string;
 
     public Description: string;
 
-    public Version: Version[];
+    public Versions: Version[];
 
     public ImageUrl: string;
 
     public SourceUrl: string;
-
-    public Date: Date;
 }
--- src/model/version.model.ts
+++ src/model/version.model.ts
@@ -1,4 +1,7 @@
 export class Version{
+
+    public ReleaseOrder : number;
+
     public VersionNumber : string;
 
     public DownloadLink : string
