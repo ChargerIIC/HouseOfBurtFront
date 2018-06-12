925a72ffb099c4b88a7255842432f3d6962abb8d
VBurt <ChargerIIC@yahoo.com>
2018-06-11 19:22:50 -0700

    Contact Page UI

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 /**
  * Generated class for the ContactPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-contact',
@@ -15,11 +14,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isSubmitted: boolean;
+  registerForm:FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb:FormBuilder, private alertCtrl: AlertController) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    },{});
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactPage');
+  }
+
+  onSubmit(){
+    this.isSubmitted = true;
+    console.log('onSubmit');
+    console.log(this.registerForm);
+    if ((this.registerForm.valid) && (this.registerForm.value.email)) {
+      
+      
+    }
   }
 
 }
