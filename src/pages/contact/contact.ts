5ed5e64554862c036de5fc3b5428fb275f7a28d3
VBurt <ChargerIIC@yahoo.com>
2018-06-12 12:31:08 -0700

    Contact Form update

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 /**
  * Generated class for the ContactPage page.
  *
@@ -17,7 +19,7 @@ export class ContactPage {
   isSubmitted: boolean;
   registerForm:FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private fb:FormBuilder, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb:FormBuilder, private alertCtrl: AlertController, public http:HttpClient) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     },{});
@@ -29,10 +31,13 @@ export class ContactPage {
   onSubmit(){
     this.isSubmitted = true;
     console.log('onSubmit');
-    console.log(this.registerForm);
+    console.log(this.registerForm.value);
     if ((this.registerForm.valid) && (this.registerForm.value.email)) {
-      
-      
+      var body = {
+        email: this.registerForm.value.email,
+        content: this.registerForm.value.content
+      };
+      this.http.post('https://us-central1-houseofburt-f4b61.cloudfunctions.net/sendEmailContact', body);      
     }
   }
 
